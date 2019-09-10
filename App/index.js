e1007cd8abc54b43d7c2e65a23d14de2ce757d30
Spencer Carli <spencercarli@gmail.com>
2019-09-10 10:25:30 -0500

    Starter app

--- App/index.js
+++ App/index.js
@@ -7,10 +7,6 @@ import {
   ActivityIndicator,
 } from 'react-native';
 
-import { ArticleRow } from './components/ArticleRow';
-import { client } from './graphql/client';
-import { TopHeadlines } from './graphql/queries';
-
 const styles = StyleSheet.create({
   headerText: {
     color: '#ff8d01',
@@ -26,24 +22,6 @@ class App extends React.Component {
   state = {
     articles: [],
     loading: true,
-  };
-
-  componentDidMount() {
-    this.requestApollo();
-  }
-
-  requestApollo = () => {
-    client
-      .query({ query: TopHeadlines, variables: { category: 'technology' } })
-      .then(response => {
-        this.setState({
-          articles: response.data.headlines.articles,
-          loading: response.loading,
-        });
-      })
-      .catch(err => {
-        console.log('err', err);
-      });
   };
 
   renderFooter = () => {
@@ -62,14 +40,12 @@ class App extends React.Component {
           ListHeaderComponent={
             <Text style={styles.headerText}>Top Headlines</Text>
           }
-          renderItem={({ item, index }) => (
-            <ArticleRow index={index} {...item} />
-          )}
-          keyExtractor={item => `${item.publishedAt}-${item.title}`}
+          renderItem={() => null}
           ListFooterComponent={this.renderFooter()}
         />
       </SafeAreaView>
     );
   }
 }
+
 export default App;
